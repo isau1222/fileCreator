652922275b515c472ddb783fe2e21ff449c1d67c
Denis Karabaza <denis.karabaza@gmail.com>
2016-10-29 11:28:44 +0300

    Adjust responses to JSON format

--- api/responses/ok.js
+++ api/responses/ok.js
@@ -4,57 +4,36 @@
  * Usage:
  * return res.ok();
  * return res.ok(data);
- * return res.ok(data, 'auth/login');
+ * return res.ok(data, details);
  *
- * @param  {Object} data
- * @param  {String|Object} options
- *          - pass string to render specified view
+ * @param  {Any} data
+ *          - Data to send along the `err.message`.
+ * @param  {Any} details
+ *          - Details about the request, will NOT be sent to user.
  */
 
-module.exports = function sendOK (data, options) {
-
+module.exports = function ok(data, details) {
   // Get access to `req`, `res`, & `sails`
   var req = this.req;
   var res = this.res;
-  var sails = req._sails;
 
-  sails.log.silly('res.ok() :: Sending 200 ("OK") response');
+  // @TODO: log metrics
 
   // Set status code
   res.status(200);
 
-  // If appropriate, serve data as JSON(P)
-  // If views are disabled, revert to json
-  if (req.wantsJSON || sails.config.hooks.views === false) {
-    return res.jsonx(data);
+  // Only include errors in response if application environment
+  // is not set to 'production'.  In production, we shouldn't
+  // send back any identifying information about errors.
+  if (sails.config.environment === 'production' && sails.config.keepResponseErrors !== true) {
+    return res.json({
+      data: data,
+    });
   }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
-  // Attempt to prettify data for views, if it's a non-error object
-  var viewData = data;
-  if (!(viewData instanceof Error) && 'object' == typeof viewData) {
-    try {
-      viewData = require('util').inspect(data, {depth: null});
-    }
-    catch(e) {
-      viewData = undefined;
-    }
+  else {
+    return res.json({
+      data: data,
+      details: details,
+    });
   }
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: viewData, title: 'OK' });
-  }
-
-  // If no second argument provided, try to serve the implied view,
-  // but fall back to sending JSON(P) if no view can be inferred.
-  else return res.guessView({ data: viewData, title: 'OK' }, function couldNotGuessView () {
-    return res.jsonx(data);
-  });
-
 };
