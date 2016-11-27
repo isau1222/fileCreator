851055d6d4a4dd8599429a53b89687e43758647a
Denis Karabaza <denis.karabaza@gmail.com>
2016-11-27 03:45:36 +0300

    Fix typo

--- api/controllers/v1/AuthController.js
+++ api/controllers/v1/AuthController.js
@@ -1,5 +1,5 @@
 /**
- * UserController
+ * AuthController
  *
  * @description :: Server-side logic for managing users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
