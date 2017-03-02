c887b686f870a5e5e0c736893d29daed078ae575
karabaza <karabaza@neolant.ru>
2017-03-02 12:44:43 +0300

    Improve client vm mounting

--- webapp/src/main-client.js
+++ webapp/src/main-client.js
@@ -4,6 +4,16 @@ var api = main.api;
 var router = main.router;
 var store = main.store;
 
+// === //
+
+var targetSelector = '#' + process.env.APP_ID;
+var target = document.querySelector(targetSelector);
+if (!target) {
+  throw new Error('The root element ' + targetSelector + ' is not present');
+}
+
+// === //
+
 // @NOTE: state rehydration
 // @TODO: codepath when no rehydration occures (mimic the SSR codepath)
 store.replaceState(window.__INITIAL_STATE__);
@@ -14,5 +24,5 @@ var vm = new Vue(app);
 // @NOTE: wait until router has resolved possible async hooks
 router.onReady(function() {
   // @NOTE: mount the instance to the dom
-  vm.$mount('#' + process.env.APP_ID);
+  vm.$mount(target);
 });
