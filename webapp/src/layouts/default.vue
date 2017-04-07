19b1a73d841f1f3b4a4a1f45a3ae96845605cf9f
karabaza <karabaza@neolant.ru>
2017-04-07 18:02:51 +0300

    Introduce layouts

---
+++ webapp/src/layouts/default.vue
@@ -0,0 +1,31 @@
+<template lang="jade">
+  div(:id="APP_ID")
+    h3 Crumbs
+    pre {{ $crumbs }}
+    ul
+      li: router-link(to="/") Dashboard
+      li: router-link(to="/auth") Auth
+      li
+        router-link(to="/reports") Reports
+        ul
+          li(v-for="io in 5")
+            router-link(:to="{ name: 'report', params: { reportId: io } }") Report \#{{ io }}
+    router-view
+</template>
+
+<style lang="sass">
+</style>
+
+<script>
+  module.exports = {
+
+    name: 'DefaultLayout',
+
+    data: function() {
+      return {
+        APP_ID: process.env.APP_ID, // @IMPORTANT
+      };
+    },
+
+  };
+</script>
--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,10 +1,43 @@
-var vueModules = require.context('@/vue-modules', true, /\.js$/);
-_.each(vueModules.keys(), path => vueModules(path));
+var each = require('lodash/each');
 
-var rootVue = require('@/pages/app.vue');
-module.exports = _.extend({}, rootVue, {
+var vueModules = require.context('@/vue-modules', true, /\.js$/);
+each(vueModules.keys(), path => vueModules(path));
+
+var defaultLayout = require('@/layouts/default');
+
+module.exports = {
+
   api: require('@/api'),
   store: require('@/store'),
   router: require('@/router'),
   routeData: require('@/route-data'),
-});
+
+  // @NOTE: this will be called during SSR
+  // @NOTE: this is a static method of the component, and can not access `this`,
+  //        because the instance may not exist during SSR
+  // @NOTE: the only way to share information between server and client
+  //        is to pass it through the vuex store, so it's likely that
+  //        only store actions will be called here
+  // @NOTE: restrain from cross-origin requests, because it's awkward
+  //        to perform them during ssr
+  // @NOTE: this preFetch should fetch all the data necessary for resolving
+  //        router guards
+  // @NOTE: in most components preFetch has a signature of `(store, route)`,
+  //        but for the root component preFetch happens before the router is
+  //        ignited, so it only gets `(store)`
+  preFetch: function(store) {
+    return Promise.resolve()
+      .then(function() {
+        return store.dispatch('auth/update');
+      });
+  },
+
+  render: function(h) {
+    // @TODO: when vue-router supports getting proper matched records,
+    //        move the `layout` field to the route record instead of route record meta
+
+    var layout = this.$route.meta.layout || defaultLayout;
+    return h(layout);
+  },
+
+};
