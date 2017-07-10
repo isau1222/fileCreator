7f4e34412ead4cf7a70cfe9b40916f919cbfe575
andreascag <andreas97fm@mail.ru>
2017-07-10 16:00:07 +0300

    add claim letters

--- assets/fileCreator.js
+++ assets/fileCreator.js
@@ -3,7 +3,7 @@ var Docxtemplater = require('docxtemplater');
 var fs = require('fs');
 var path = require('path');
 
-const pathToTemplates = __dirname + '/assets/templates/';
+const pathToTemplates = __dirname + '/templates/';
 
 var method_FileNameDictionary = {
   'expertConclusion': 'Экспертное заключение.docx',
@@ -15,17 +15,16 @@ function saveBuffer(buf, fileName) {
 
 function readDoc(fileName) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.resolve(__dirname, pathToTemplates + fileName), 'binary',
+    fs.readFile(path.resolve(pathToTemplates + fileName), 'binary',
       (err, data) => {
         if (err) {
-          console.log('rejhjjj');
           return reject(err);
         }
         var zip = new JSZip(data);
         var doc = new Docxtemplater();
         resolve(doc.loadZip(zip));
-      })
-  })
+      });
+  });
 }
 
 function createDocBuffer(fileName, json, jsonObjConverter, nameCreator) {
@@ -61,7 +60,10 @@ function createDocBuffer(fileName, json, jsonObjConverter, nameCreator) {
 }
 
 function nameNormalizer(name) {
+  if (!name)
+    return '';
   return name.replace(/[/\*?|:<>"]{1}/g, '_');
+
 }
 
 /**
@@ -90,12 +92,82 @@ function printExpertConclusion(json) {
     return data;
   };
   var nameCreator = (data) => {
-    return 'Экспертное заключение ' + nameNormalizer(data.expertConclusionNumber) + '.docx'
+    return 'Экспертное заключение' + nameNormalizer(data.expertConclusionNumber) + '.docx'
   };
 
   var fileName = method_FileNameDictionary['expertConclusion'];
   return createDocBuffer(fileName, json, converter, nameCreator)
 }
+
+/**
+ * 
+ * @param {object} json 
+ * @param {string} json.verifyAgencyFullname - [Проверка.Орган, осущ.проверку.Наименование]
+ * @param {string} json.verifyAgencyAddress - [Проверка.Орган, осущ.проверку.Адрес субъекта ГД]
+ * @param {string} json.verifyAgencyPhone - [Проверка.Орган, осущ.проверку. телефон субъекта ГД]
+ * @param {string} json.claimLetterDate - [Ущерб.Дата (претенз. письма)]
+ * @param {string} json.claimNumber - [Ущерб.Номер (претенз. письма)
+ * @param {string} json.claimLetterToAddress - [Ущерб.В адрес]
+ * @param {string} json.claimLetterToName - [Ущерб.На имя]
+ * @param {string} json.verifyAgencyName - [Проверка.Орган, осущ. проверку]
+ * @param {string} json.verificationTermsFrom - [Результат. Сроки фактического проведения проверки: от]
+ * @param {string} json.verificationTermsTo - [Сроки фактического проведения проверки: до] 
+ * @param {string} json.typeOfVerification - [Проверка.вид проверки]
+ * @param {string} json.companyForVerificate - [Проверка.Предприятие]
+ * @param {string} json.placeOfVerification - [Результат.фактическое место проведения проверки]
+ * @param {string} json.damageSummary - [Ущерб.Краткое содержание]
+ * @param {string} json.verifyAgencyLeader - [Проверка.Орган, осущ. проверку.Руководителя]
+ * @param {string} json.fullNameOfCurrentUser - [ФИО текущего пользователя]
+ * @param {string} json.phoneOfCurrentUser - [Субъект ГД текущего пользователя.телефон]
+ * @return {Promise} next .then get {buf, fileName}
+ */
+function printClaimLetter(json) {
+
+  var converter = undefined;
+  var nameCreator = (data) => {
+    return 'Претензионное письмо' + nameNormalizer(data.claimNumber)
+      +' в адрес '+nameNormalizer(data.claimLetterToAddress) + '.docx';
+  };
+
+  var fileName = method_FileNameDictionary['expertConclusion'];
+  return createDocBuffer(fileName, json, converter, nameCreator)
+}
+
+/**
+ * 
+ * @param {object} json 
+ * @param {string} json.verifyAgencyFullname - [Проверка.Орган, осущ.проверку.Наименование]
+ * @param {string} json.verifyAgencyAddress - [Проверка.Орган, осущ.проверку.Адрес субъекта ГД]
+ * @param {string} json.verifyAgencyPhone - [Проверка.Орган, осущ.проверку. телефон субъекта ГД]
+ * @param {string} json.claimLetterDate - [Ущерб.Дата (претенз. письма)]
+ * @param {string} json.claimNumber - [Ущерб.Номер (претенз. письма)
+ * @param {string} json.claimLetterToAddress - [Ущерб.В адрес]
+ * @param {string} json.claimLetterToName - [Ущерб.На имя]
+ * @param {string} json.verifyAgencyName - [Проверка.Орган, осущ. проверку]
+ * @param {string} json.verificationTermsFrom - [Результат. Сроки фактического проведения проверки: от]
+ * @param {string} json.verificationTermsTo - [Сроки фактического проведения проверки: до] 
+ * @param {string} json.typeOfVerification - [Проверка.вид проверки]
+ * @param {string} json.companyForVerificate - [Проверка.Предприятие]
+ * @param {string} json.placeOfVerification - [Результат.фактическое место проведения проверки]
+ * @param {string} json.damageSummary - [Ущерб.Краткое содержание]
+ * @param {string} json.verifyAgencyLeader - [Проверка.Орган, осущ. проверку.Руководителя]
+ * @param {string} json.fullNameOfCurrentUser - [ФИО текущего пользователя]
+ * @param {string} json.phoneOfCurrentUser - [Субъект ГД текущего пользователя.телефон]
+ * @return {Promise} next .then get {buf, fileName}
+ */
+function printClaimLetter(json) {
+
+  var converter = undefined;
+  var nameCreator = (data) => {
+    return 'Претензионное письмо' + nameNormalizer(data.claimNumber)
+      +' в адрес '+nameNormalizer(data.claimLetterToAddress) + '.docx';
+  };
+
+  var fileName = method_FileNameDictionary['expertConclusion'];
+  return createDocBuffer(fileName, json, converter, nameCreator)
+}
+
 module.exports = {
   printExpertConclusion,
+  printClaimLetter,
 };
