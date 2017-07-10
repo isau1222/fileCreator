590e35df83de6bdfa13bed6883e5aef1f1d7815d
andreascag <andreas97fm@mail.ru>
2017-07-10 16:00:47 +0300

    Fix trash

--- assets/fileCreator.js
+++ assets/fileCreator.js
@@ -133,39 +133,7 @@ function printClaimLetter(json) {
   return createDocBuffer(fileName, json, converter, nameCreator)
 }
 
-/**
- * 
- * @param {object} json 
- * @param {string} json.verifyAgencyFullname - [Проверка.Орган, осущ.проверку.Наименование]
- * @param {string} json.verifyAgencyAddress - [Проверка.Орган, осущ.проверку.Адрес субъекта ГД]
- * @param {string} json.verifyAgencyPhone - [Проверка.Орган, осущ.проверку. телефон субъекта ГД]
- * @param {string} json.claimLetterDate - [Ущерб.Дата (претенз. письма)]
- * @param {string} json.claimNumber - [Ущерб.Номер (претенз. письма)
- * @param {string} json.claimLetterToAddress - [Ущерб.В адрес]
- * @param {string} json.claimLetterToName - [Ущерб.На имя]
- * @param {string} json.verifyAgencyName - [Проверка.Орган, осущ. проверку]
- * @param {string} json.verificationTermsFrom - [Результат. Сроки фактического проведения проверки: от]
- * @param {string} json.verificationTermsTo - [Сроки фактического проведения проверки: до] 
- * @param {string} json.typeOfVerification - [Проверка.вид проверки]
- * @param {string} json.companyForVerificate - [Проверка.Предприятие]
- * @param {string} json.placeOfVerification - [Результат.фактическое место проведения проверки]
- * @param {string} json.damageSummary - [Ущерб.Краткое содержание]
- * @param {string} json.verifyAgencyLeader - [Проверка.Орган, осущ. проверку.Руководителя]
- * @param {string} json.fullNameOfCurrentUser - [ФИО текущего пользователя]
- * @param {string} json.phoneOfCurrentUser - [Субъект ГД текущего пользователя.телефон]
- * @return {Promise} next .then get {buf, fileName}
- */
-function printClaimLetter(json) {
 
-  var converter = undefined;
-  var nameCreator = (data) => {
-    return 'Претензионное письмо' + nameNormalizer(data.claimNumber)
-      +' в адрес '+nameNormalizer(data.claimLetterToAddress) + '.docx';
-  };
-
-  var fileName = method_FileNameDictionary['expertConclusion'];
-  return createDocBuffer(fileName, json, converter, nameCreator)
-}
 
 module.exports = {
   printExpertConclusion,
