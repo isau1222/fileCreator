0c00045a0a6577a5e320abe77377bdd1439b733f
crutch12 <criitch@yandex.ru>
2017-07-11 13:34:13 +0300

    edit template reid

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -10,18 +10,15 @@ var assets = require('../../assets/fileCreator/index.js');
 module.exports = {
   getFile: function(req, res) {
 
-    // var type = req.param('type');
-    // var json = req.param('json');
+    var type = req.param('type');
+    var json = req.param('json');
 
-        // var type = req.param('type');
-    var json = {verification: ''};
+    if (!type || !json){
+      return res.apiBadRequest();
+      // return res.redirect('back');
+    }
 
-    // if (!type || !json){
-    //   return res.apiBadRequest();
-    //   // return res.redirect('back');
-    // }
-
-    assets.printFromType('actOfVerification', json)
+    assets.printFromType(type, json)
       .then(({buf, fileName})=>{
         res.attachment(fileName);
 
